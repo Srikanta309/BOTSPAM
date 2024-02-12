2bfea42944098094cafa7c5907489eb99e1f94e4
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2024-02-12 10:03:04 +0530

    Update clients.py

--- LegendGirl/core/clients.py
+++ LegendGirl/core/clients.py
@@ -14,7 +14,7 @@ group_username = "@LegendSpamBot"
 logging.basicConfig(format="%(levelname)s  %(message)s", level=logging.INFO)
 
 
-if  in BOT_TOKEN:
+if  BOT_TOKEN:
     Client1 = call(
         "LegendSpam",
         api_id=APP_ID,
